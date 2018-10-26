6d1066df9e5e4e318213275e5a8c1ea4efd341a6
Joinson <524604033@qq.com>
2018-10-26 11:04:34 +0800

    add

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import './assets/styles/reset.css'
+// 解决移动端1px的问题 例：1px在二倍屏会显示成2px
+import './assets/styles/border.css'
+import fastClick from 'fastClick'
 Vue.config.productionTip = false
-
+//fastClick解决移动端click事件延迟300ms
+fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import home from '@/pages/home'
+import list from '@/pages/list'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+  routes: [{
+    path: '/',
+    name: 'home',
+    component: home
+  }, {
+    path: '/list',
+    name: 'list',
+    component: list
+  }
   ]
 })
